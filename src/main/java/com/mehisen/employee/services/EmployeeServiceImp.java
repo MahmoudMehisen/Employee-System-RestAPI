8d5358b12631c1691ec80fdfe08cc6f62cbe15da
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2022-11-02 21:43:25 +0200

    add delete employee

--- src/main/java/com/mehisen/employee/services/EmployeeServiceImp.java
+++ src/main/java/com/mehisen/employee/services/EmployeeServiceImp.java
@@ -36,4 +36,11 @@ public class EmployeeServiceImp implements EmployeeService {
         List<Employee> employees = employeeEntities.stream().map(emp -> new Employee(emp.getId(), emp.getFirstName(), emp.getLastName(), emp.getEmailId())).collect(Collectors.toList());
         return employees;
     }
+
+    @Override
+    public boolean deleteEmployee(Long id) {
+        EmployeeEntity employee  = employeeRepository.findById(id).get();
+        employeeRepository.delete(employee);
+        return true;
+    }
 }
