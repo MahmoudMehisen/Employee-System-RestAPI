685821ada183245ef70a1ecdabad3ebefcd1c494
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2022-11-02 21:59:48 +0200

    add get and update employee

--- src/main/java/com/mehisen/employee/services/EmployeeServiceImp.java
+++ src/main/java/com/mehisen/employee/services/EmployeeServiceImp.java
@@ -39,8 +39,27 @@ public class EmployeeServiceImp implements EmployeeService {
 
     @Override
     public boolean deleteEmployee(Long id) {
-        EmployeeEntity employee  = employeeRepository.findById(id).get();
+        EmployeeEntity employee = employeeRepository.findById(id).get();
         employeeRepository.delete(employee);
         return true;
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
+    }
+
+    @Override
+    public Employee updateEmployee(Long id, Employee employee) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        employeeEntity.setEmailId(employee.getEmailId());
+        employeeEntity.setFirstName(employee.getFirstName());
+        employeeEntity.setLastName(employee.getLastName());
+
+        employeeRepository.save(employeeEntity);
+        return employee;
     }
 }
